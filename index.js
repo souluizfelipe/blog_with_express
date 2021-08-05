83bae8196b6d123105cc96373cfa5a6a2c76b5bc
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-08-05 09:54:11 -0300

    create post method to get data from new post form and store in posts.json

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// const { timeLog } = require('console');
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 
 const app = express();
@@ -7,6 +9,8 @@ app.set('view engine', 'ejs');
 
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extende: true }));
+
 
 app.get('/', (req, res) => {
   res.render('index', {
@@ -17,8 +21,26 @@ app.get('/', (req, res) => {
 app.get('/create-post', (req, res) => {
   res.render('create-post', {
     title: 'Novo Post - Duh!'
-  })
-})
+  });
+});
+
+app.post('/posted', (req, res) => {
+  const { title, text } = req.body;
+
+  const data = fs.readFileSync('./stored/posts.json');
+  const posts = JSON.parse(data);
+
+  posts.push({
+    title,
+    text
+  });
+
+  const postsString = JSON.stringify(posts);
+  fs.writeFileSync('./stored/posts.json', postsString);
+
+  
+  res.redirect('/');
+});
 
 
 const port = process.env.PORT || 8080;
