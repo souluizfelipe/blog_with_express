bfd809a0d86ea5d821550a71cb34cb2a06574483
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-08-05 10:36:36 -0300

    fixed css not reloading after post method by fixing function on app.listen()

--- index.js
+++ index.js
@@ -2,26 +2,30 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-
 const app = express();
+
 app.set('view engine', 'ejs');
 
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extende: true }));
 
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.render('index', {
     title: 'Duh!',
-    posts: JSON.parse(fs.readFileSync('./stored/posts.json')),
+    posts: JSON.parse(fs.readFileSync('./stored/posts.json')).reverse(),
   });
 });
 
 app.get('/create-post', (req, res) => {
+  const { c } = req.query;
+
   res.render('create-post', {
     title: 'Novo Post - Duh!',
+    cadastrado: c,
   });
+
 });
 
 app.post('/posted', (req, res) => {
@@ -38,11 +42,10 @@ app.post('/posted', (req, res) => {
   const postsString = JSON.stringify(posts);
   fs.writeFileSync('./stored/posts.json', postsString);
 
-  
-  res.redirect('/');
+  res.redirect('/create-post?c=1');
 });
 
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, console.log('working...'));
+app.listen(port, () => console.log(`Port ${port} working...`));
