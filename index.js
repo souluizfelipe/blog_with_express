4c5f7f4654a593381490edae6717c277ef50deef
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-08-05 08:53:16 -0300

    create create-post page and add route to index.js

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
 
+const app = express();
 app.set('view engine', 'ejs');
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    title: 'Duh!'
+  });
 });
+
+app.get('/create-post', (req, res) => {
+  res.render('create-post', {
+    title: 'Novo Post - Duh!'
+  })
+})
+
 
 const port = process.env.PORT || 8080;
 
